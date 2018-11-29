abe90afe252bca5c443467ed115f76acca65bb08
Ryan Miyahara <rmiyahara144@gmail.com>
2018-11-28 20:35:22 -0800

    Added argument check

--- lab3b.py
+++ lab3b.py
@@ -4,9 +4,25 @@
 
 import sys
 import csv
+import pandas
 
 def main():
-    print("Hello world")
+    #Check argument
+    if (len(sys.argv) != 2):
+        print("Please use this program in the following format: ./lab3 CSVFILE", file=sys.stderr)
+        sys.exit(1)
+    filename = sys.argv[1]
+    if (len(filename) < 4):
+        print("Please use this program in the following format: ./lab3 CSVFILE", file=sys.stderr)
+        sys.exit(1)
+    format = filename[len(filename) - 4:]
+    if (format != ".csv"):
+        print("Please use this program in the following format: ./lab3 CSVFILE", file=sys.stderr)
+        sys.exit(1)
+
+    #Take in data
+    data = pandas.read_csv(filename)
+
     sys.exit(0) #Successful exit
 
 if __name__ == "__main__":
